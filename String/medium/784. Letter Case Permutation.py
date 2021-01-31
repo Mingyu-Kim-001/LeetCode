614bff9f1abd04151241564b6fc01fa19233a83c
Hugo <hugo.min@kakaocorp.com>
2021-01-31 10:36:43 +0900

    add solution

---
+++ String/medium/784. Letter Case Permutation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def letterCasePermutation(self, S: str) -> List[str]:
+        result = [S]
+        for i,c in enumerate(S):
+            if c.isalpha():
+                curLen = len(result)
+                for j in range(curLen):
+                    s = result[j]
+                    result.append(s[:i] + 
+                                  (c.upper() if c.islower() else c.lower()) + 
+                                  s[i+1:]
+                                 )
+        return result
