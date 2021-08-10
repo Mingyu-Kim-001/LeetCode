94693b27a3f739dd4fde1fc47eaada6ac3199347
Mingyu-Kim-001 <>
2021-08-10 18:11:50 +0900

    add solutions 415, 926

--- Arrays/medium/926. Flip String to Monotone Increasing.py
+++ Arrays/medium/926. Flip String to Monotone Increasing.py
@@ -1,14 +1,13 @@
-#Time: O(n)
 class Solution:
-    def minFlipsMonoIncr(self, S: str) -> int:
-        n_zero = n_one = 0
-        for c in S:
-            if c=="0":n_zero+=1
-            else:n_one+=1
-        cur_n_zero = cur_n_one = 0
-        result = n_zero
-        for c in S:
-            if c=="0":cur_n_zero+=1
-            else:cur_n_one+=1
-            result = min(result,cur_n_one + n_zero - cur_n_zero)
-        return result
+    def minFlipsMonoIncr(self, s: str) -> int:
+        front_1 = 0
+        back_0 = sum([int(c == "0") for c in s])
+        ans = front_1 + back_0
+        for c in s:
+            if c == "1":
+                front_1 += 1
+            else:
+                back_0 -= 1
+            ans = min(ans, back_0 + front_1)
+        return ans
+            
