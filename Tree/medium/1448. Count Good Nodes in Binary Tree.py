25ce7efeeb59f503f653c8f4c58ddb47d7eb5f2e
Mingyu-Kim <>
2021-08-17 20:47:36 +0900

    add a solution of 1448

--- Tree/medium/1448. Count Good Nodes in Binary Tree.py
+++ Tree/medium/1448. Count Good Nodes in Binary Tree.py
@@ -4,15 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-#Time: O(n)
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def DFS(node,maxVal):
-            if not node: return 0
-            count = 0
-            if maxVal<=node.val:
-                maxVal = node.val
-                count = 1
-            return count + DFS(node.left,maxVal) + DFS(node.right,maxVal)
-        return DFS(root,float("-inf"))
+        
+        def DFS(node, max_val):
+            if not node:
+                return 0
+            return int(max_val <= node.val) + DFS(node.left, max(max_val, node.val)) + DFS(node.right, max(max_val, node.val))
+        
+        return DFS(root, root.val)
